f04718782436e9cb16d05aa0c95c1ee23e152de9
Gianluca Colaiacomo <g.colaiacomo@gmail.com>
2025-08-21 15:39:14 +0200

    fix: Correggo il nome dell'import del plugin in MainPreset

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -22,7 +22,7 @@ import {
   Preset,
   VizType,
 } from '@superset-ui/core';
-import { AdvancedTableCommodities } from '../../../plugins/superset-plugin-viz-advanced-table/src';
+import SupersetPluginVizAdvancedTable from '../../../plugins/superset-plugin-viz-advanced-table/src';
 import CalendarChartPlugin from '@superset-ui/legacy-plugin-chart-calendar';
 import ChordChartPlugin from '@superset-ui/legacy-plugin-chart-chord';
 import CountryMapChartPlugin from '@superset-ui/legacy-plugin-chart-country-map';
@@ -104,7 +104,7 @@ export default class MainPreset extends Preset {
       name: 'Legacy charts',
       presets: [new DeckGLChartPreset()],
       plugins: [
-        new AdvancedTableCommodities().configure({ key: 'advanced-table-commodities' }),
+        new SupersetPluginVizAdvancedTable().configure({ key: 'advanced-table' }),
         new BigNumberChartPlugin().configure({ key: VizType.BigNumber }),
         new BigNumberTotalChartPlugin().configure({
           key: VizType.BigNumberTotal,
